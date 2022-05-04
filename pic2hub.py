364ab22777f80dea913783e48ae27c8304846e35
yxxx <i@sometimenaive.com>
2022-05-04 09:35:42 +0800

    add icon_256x256.png.webp

--- pic2hub.py
+++ pic2hub.py
@@ -8,6 +8,9 @@ from asyncio.subprocess import DEVNULL
 from PIL import Image
 
 def upload(images):
+    if len(images) < 1:
+        sys.exit()
+
     for image in images:
         createdate = time.localtime(os.stat(image).st_birthtime)
         year = createdate.tm_year
@@ -15,15 +18,14 @@ def upload(images):
         dstpath = f"/Users/waderwu/blog-img/{year}/{month}"
         if not os.path.isdir(dstpath):
             os.makedirs(dstpath)
-        imgname = os.path.basename(image)
         if os.path.isfile(image):
+            imgname = os.path.basename(image)
             webp = f"{dstpath}/{imgname}.webp"
             Image.open(image).convert("RGB").save(webp, "webp")
-            imgname = os.path.basename(image)
             imgurl = f"https://raw.githubusercontent.com/waderwu/blog-img/master/{year}/{month}/{imgname}.webp"
             print(imgurl)
         
-    subprocess.check_output(["bash", "-c", f"cd /Users/waderwu/blog-img/; git pull ; git add . ; git commit -m 'add {year}-{month}' ; git push"], stderr=DEVNULL)
+    subprocess.check_output(["bash", "-c", f"cd /Users/waderwu/blog-img/; git pull ; git add . ; git commit -m 'add {imgname}.webp' ; git push"], stderr=DEVNULL)
 
 
 if __name__ == "__main__":
